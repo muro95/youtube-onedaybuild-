f87bf61f171c3be269a15a655e8c90123930b46c
muro95 <sonbena95@gmail.com>
2020-06-16 12:21:37 -0700

    Constants file

--- youtube-onedaybuild/Constants.swift
+++ youtube-onedaybuild/Constants.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+struct Constants {
+    
+    static var API_KEY = "AIzaSyAZ6f2jlNE0Xv9fU6sYzOQ0KPLnejGsvIo"
+    static var PLAYLIST_ID = "PLjNnuNwnTfTYQPj6ipUVXTXr0vaDHGalB"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
