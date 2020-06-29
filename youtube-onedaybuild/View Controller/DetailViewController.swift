c2d3f18dc34e24c8d19c28a2ef12ed63befbf014
muro95 <sonbena95@gmail.com>
2020-06-28 21:41:29 -0700

    added detailViewController, segue to detail, and webview in detailViewController

---
+++ youtube-onedaybuild/View Controller/DetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DetailViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Alex Truong on 6/28/20.
+//  Copyright © 2020 Alex Truong. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    var video: Video?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Clear the fields
+        titleLabel.text = ""
+        dateLabel.text = ""
+        textView.text = ""
+        
+        // Check if there's a video
+        guard video != nil else {
+            return
+        }
+        
+        // Create the embed URL
+        let embedUrlString = Constants.YT_EMBED_URL + video!.videoId
+        
+        
+        // Load it into the webview
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        // Set the title
+        titleLabel.text = video!.title
+        
+        // Set the date
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = df.string(from: video!.published)
+        
+        // Set the description
+        textView.text = video!.description
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
